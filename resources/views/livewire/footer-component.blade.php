df36e61a9a54559c8a5e27a394d3665b930f1f7a
Sa3edAhmed7 <saeidahmed774@gmail.com>
2022-11-26 01:54:18 +0200

    first

--- resources/views/livewire/footer-component.blade.php
+++ resources/views/livewire/footer-component.blade.php
@@ -235,7 +235,7 @@
 			<div class="coppy-right-box">
 				<div class="container">
 					<div class="coppy-right-item item-left">
-						<p class="coppy-right-text">Copyright © 2022 Surfside Media. All rights reserved</p>
+						<p class="coppy-right-text">Copyright © 2022 Sa3ed Ahmed. All rights reserved</p>
 					</div>
 					<div class="coppy-right-item item-right">
 						<div class="wrap-nav horizontal-nav">
