628fa3a092c92b521f372417a62338ce4d9d7386
Sa3edAhmed7 <95840200+Sa3edAhmed7@users.noreply.github.com>
2022-12-04 19:08:47 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -36,7 +36,7 @@ class AppServiceProvider extends ServiceProvider
             'phone' => '123456789',
             'phone2' => '123456789',
             'address' => 'test',
-            'map' => 'test',
+            'map' => 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3456.022633610082!2d31.14824828550972!3d29.9787795286279!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1458458b62a253b7%3A0x9b911889aabe1cbd!2zMTQg2KfZhNij2YXZitix2Kkg2YHYp9iv2YrYqdiMINmG2LLZhNipINin2YTYs9mF2KfZhtiMINin2YTZh9ix2YXYjCDYp9mE2KzZitiy2KkgMTI1NTc!5e0!3m2!1sar!2seg!4v1670173436918!5m2!1sar!2seg',
             'twiter' => 'twiter',
             'facebook' => 'facebook',
             'pinterest' => 'pinterest',
