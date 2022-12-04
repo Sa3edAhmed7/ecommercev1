f082842a3f8368dca7b9118805470db8473d8a78
Sa3edAhmed7 <95840200+Sa3edAhmed7@users.noreply.github.com>
2022-12-04 19:18:29 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -36,7 +36,7 @@ class AppServiceProvider extends ServiceProvider
             'phone' => '123456789',
             'phone2' => '123456789',
             'address' => 'test',
-            'map' => 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3456.022633610082!2d31.14824828550972!3d29.9787795286279!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1458458b62a253b7%3A0x9b911889aabe1cbd!2zMTQg2KfZhNij2YXZitix2Kkg2YHYp9iv2YrYqdiMINmG2LLZhNipINin2YTYs9mF2KfZhtiMINin2YTZh9ix2YXYjCDYp9mE2KzZitiy2KkgMTI1NTc!5e0!3m2!1sar!2seg!4v1670173436918!5m2!1sar!2seg',
+            'map' => 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d864.0097378559639!2d31.146849570823644!3d29.978310560553858!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x145845d72441f427%3A0x8d70bea5eb5def25!2z2YXYudmF2YQg2KfYqNmIINin2YTZh9mI2YQg2YTZhNiq2K3Yp9mK2YQg2KfZhNi32KjZitmH!5e0!3m2!1sar!2seg!4v1670174017075!5m2!1sar!2seg',
             'twiter' => 'twiter',
             'facebook' => 'facebook',
             'pinterest' => 'pinterest',
