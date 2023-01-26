07eb5e24d2c33257273be6d97f6a62ba2620e6c8
Sa3edAhmed7 <95840200+Sa3edAhmed7@users.noreply.github.com>
2023-01-26 21:35:02 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -38,19 +38,19 @@ class AppServiceProvider extends ServiceProvider
             'phone2' => '123456789',
             'address' => 'test',
             'map' => 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d864.0097378559639!2d31.146849570823644!3d29.978310560553858!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x145845d72441f427%3A0x8d70bea5eb5def25!2z2YXYudmF2YQg2KfYqNmIINin2YTZh9mI2YQg2YTZhNiq2K3Yp9mK2YQg2KfZhNi32KjZitmH!5e0!3m2!1sar!2seg!4v1670174017075!5m2!1sar!2seg',
-<<<<<<< HEAD
+
             'twiter' => 'twiter',
             'facebook' => 'facebook',
             'pinterest' => 'pinterest',
             'instagram' => 'instagram',
             'youtube' => 'youtube',
-=======
+
             'twiter' => 'https://www.twitter.com',
             'facebook' => 'https://www.facebook.com',
             'pinterest' => 'https://www.pinterest.com',
             'instagram' => 'https://www.instagram.com',
             'youtube' => 'https://www.youtube.com'
->>>>>>> 1b84a1e (first)
+
         ]);
         });
         view()->share('setting', $setting);
